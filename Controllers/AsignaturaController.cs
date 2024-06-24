d89bffd9295377c7cd9665f3a36e2faeb09e0d78
Bladimir <jossuebgonzalez18@gmail.com>
2024-06-24 10:41:19 -0600

    Construccion

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -2,33 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Escolar.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-
-
 
 namespace Escolar.Controllers
 {
-    //[Authorize]
     public class AsignaturaController : Controller
     {
-        
         private readonly IrepositorioAsignatura _InstanciaAsignaturaRepo;
         private readonly IrepositorioCurso _InstanciaCursoRepo;
+        private readonly IrepositorioProfesor _InstanciaProfesorRepo;
 
-        public AsignaturaController(IrepositorioAsignatura variableAsignaturaRepo, IrepositorioCurso variableCursoRepo)
+        public AsignaturaController(IrepositorioAsignatura variableAsignaturaRepo, IrepositorioCurso variableCursoRepo, IrepositorioProfesor variableProfesorRepo)
         {
             _InstanciaAsignaturaRepo = variableAsignaturaRepo;
             _InstanciaCursoRepo = variableCursoRepo;
+            _InstanciaProfesorRepo = variableProfesorRepo;
         }
-        
-        public IActionResult Index() // devolver la vista index en productos
+
+        public IActionResult Index()
         {
-            
-            var asignaturas =  _InstanciaAsignaturaRepo.MostrarTodos();
+            var asignaturas = _InstanciaAsignaturaRepo.MostrarTodos();
             return View(asignaturas);
         }
 
-        public IActionResult Detalles(int Id) // traer los detalles de un producto especifico
+        public IActionResult Detalles(int Id)
         {
             var asignatura = _InstanciaAsignaturaRepo.ObtenerPorId(Id);
 
@@ -36,24 +32,20 @@ namespace Escolar.Controllers
             {
                 return NotFound();
             }
-             //var categoria = _InstanciaCategoriaRepo.ObtenerPorId(producto.CategoriaId);
-               // producto.Categoria = categoria;
 
             return View(asignatura);
         }
 
-       // [Authorize(Roles ="Administrador")]
-
-        public IActionResult Agregar() // Solicitar los datos de categoria
+        public IActionResult Agregar()
         {
-            ViewData["Cursos"] = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre");
+            ViewBag.Profesores = new SelectList(_InstanciaProfesorRepo.MostrarTodos(), "Id", "Nombre");
+            ViewBag.Cursos = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        //[Authorize(Roles ="Administrador")]
-        public IActionResult Agregar(Asignatura asignatura) // Inserta un producto con categoria
+        public IActionResult Agregar(Asignatura asignatura)
         {
             if (ModelState.IsValid)
             {
@@ -61,26 +53,26 @@ namespace Escolar.Controllers
                 _InstanciaAsignaturaRepo.Guardar();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CursosId"] = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre", asignatura.CursoId);
+            ViewBag.Profesores = new SelectList(_InstanciaProfesorRepo.MostrarTodos(), "Id", "Nombre", asignatura.ProfesorId);
+            ViewBag.Cursos = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre", asignatura.CursoId);
             return View(asignatura);
         }
-        //[Authorize(Roles ="Administrador")]
 
-        public IActionResult Editar(int Id) // Obtienen los datos de un producto especifico
+        public IActionResult Editar(int Id)
         {
             var asignatura = _InstanciaAsignaturaRepo.ObtenerPorId(Id);
             if (asignatura == null)
             {
                 return NotFound();
             }
-            ViewBag.Cursos = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre");
+            ViewBag.Profesores = new SelectList(_InstanciaProfesorRepo.MostrarTodos(), "Id", "Nombre", asignatura.ProfesorId);
+            ViewBag.Cursos = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre", asignatura.CursoId);
             return View(asignatura);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        //[Authorize(Roles ="Administrador")]
-        public IActionResult Editar(Asignatura asignatura) // Actualiza un producto especifico
+        public IActionResult Editar(Asignatura asignatura)
         {
             if (ModelState.IsValid)
             {
@@ -88,12 +80,12 @@ namespace Escolar.Controllers
                 _InstanciaAsignaturaRepo.Guardar();
                 return RedirectToAction(nameof(Index));
             }
+            ViewBag.Profesores = new SelectList(_InstanciaProfesorRepo.MostrarTodos(), "Id", "Nombre", asignatura.ProfesorId);
+            ViewBag.Cursos = new SelectList(_InstanciaCursoRepo.MostrarTodos(), "Id", "Nombre", asignatura.CursoId);
             return View(asignatura);
         }
 
-       // [Authorize(Roles ="Administrador")]
-
-        public IActionResult Delete(int id) // Devuelve los datos de un producto para eliminar 
+        public IActionResult Delete(int id)
         {
             var asignatura = _InstanciaAsignaturaRepo.ObtenerPorId(id);
             if (asignatura == null)
@@ -105,13 +97,11 @@ namespace Escolar.Controllers
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-       // [Authorize(Roles ="Administrador")]
-        public IActionResult DeleteConfirmed(int id) // Elimina el producto seleccionado
+        public IActionResult DeleteConfirmed(int id)
         {
             _InstanciaAsignaturaRepo.Eliminar(id);
-            _InstanciaCursoRepo.Guardar();
+            _InstanciaAsignaturaRepo.Guardar();
             return RedirectToAction(nameof(Index));
         }
     }
 }
-
